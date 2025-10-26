eeda39bf663ce2dfcf56e5b36ca4d4a36025efc4
Jayashree C S <154995690+Jayashree2026@users.noreply.github.com>
2025-10-26 18:27:45 +0530

    Fix import path for MongoDB configuration

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 import express from "express"
 import cors from 'cors'
 import 'dotenv/config'
-import connectDB from './config/Mongodb.js'
+import connectDB from './config/MongoDb.js'
 import connectCloudinary from './config/Cloudinary.js'
 import userRouter from './routes/userRoutes.js'
 import productRouter from './routes/productRoutes.js'
